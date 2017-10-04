7ffcb22f254726562d8710060f8e1fbcd8dbc475
acisliver <7273322@naver.com>
2017-10-05 02:11:34 +0900

    임시 원거리몹클래스

--- Abadguy.py
+++ Abadguy.py
@@ -8,17 +8,20 @@ class Abadguy(pygame.Rect):
     speed=0
     screen = None
     bullets = []
-    abadguy = pygame.image.load("resources/images/badguy.png")
+    abadguy = pygame.image.load("resources/images/abadguy.png")
 
-    def __init__(self, screen, x, y, speed):
+
+    def __init__(self, screen, x, y, speed, num):
         super().__init__(self.abadguy.get_rect())     #상위 클래스의 함수(rect)를 사용하기 위해 super()사용
         self.top = x
         self.left = y
         self.speed = speed
         self.screen = screen
 
+        self.num = num
+
     def move(self):             #원거리몹 움직임 함수
-        time=threading.Timer(1,self.move())  #1초마다 반복
+        time = threading.Timer(1,self.move())  #1초마다 반복
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -32,8 +35,19 @@ class Abadguy(pygame.Rect):
         for bullet in self.bullets:
             bullet.move()
 
+        if self.left >= 0 and self.left < 200:
+            self.left += self.speed
+        if self.left > 200:
+            self.left -= self.speed
+        if self.top > 700:
+            self.top -=self.speed
+        if self.top >= 0 and self.top < 700:
+            self.top += self.top
+
+        self.screen.blit(self.abadguy, (self.top, self.left))
+
 
 
     def shot(self):                 #화살생성함수
-        bullet = Bullet(self.screen, self.top, self.left, 30 )    #speed가 30인 화살 생성
+        bullet = Bullet(self.screen, self.top, self.left, 20 )    #speed가 30인 화살 생성
         self.bullets.append(bullet)                               #list에 arrow객체 추가
